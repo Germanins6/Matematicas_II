777a74e89a750478a8067da84b33f3b69d8b9a18
germanins6 <1996GInsua@gmail.com>
2019-10-12 20:21:17 +0200

    Partial Pivoting

--- LAB_1/LAB_1.m
+++ LAB_1/LAB_1.m
@@ -1,11 +1,13 @@
 %%Vector b,  Independent term
-b = [-4 -1 3]';
+b = [4 6 1]';
 
 %%Matrix A, Coefficients matrix
-A = [ 1 -2 1 ; 1 -3 -4 ; -2 3 -3];
+A = [ 0 2 1 ; 2 -3 4; 3 -1 5];
 
 %%Calling function
-[At, bt] = ownTriangulation(A,b);
+%%[At, bt] = ownTriangulation(A,b);
+
+[flag, At, bt] = ownTriangulationPartialPivoting(A,b);
 
 %% Not used right now
 %x = backSubs(At,b)
