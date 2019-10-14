694d2c742f3b767f069c967e30b49a79ff6d03c5
German Insua Perdomo <germanip@TR12.CAT>
2019-10-14 10:00:10 +0200

    Added Notebook

--- LAB_1/LAB_1.m
+++ LAB_1/LAB_1.m
@@ -1,13 +1,13 @@
 %%Vector b,  Independent term
-b = [1 0 1]';
+b = [1 0 1 2]';
 
 %%Matrix A, Coefficients matrix
-A = [-1 3 1 -4; 2 -6 -2 8; 1 1 1 1];
+A = [-1 3 1 -4; 2 -6 -2 8; 1 1 1 1; 2 4 1 8];
 
 %%Calling function
 %%[At, bt] = ownTriangulation(A,b);
 
-[flag, At, bt] = ownTriangulationPartialPivoting(A,b);
+[flag,At, bt] = ownTriangulationPartialPivoting(A,b);
 
 %% Not used right now
 %x = backSubs(At,b)
