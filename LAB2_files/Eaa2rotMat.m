fe8afe3ce90dafff24c2d6225e4e69998f9b0bf9
German Insua Perdomo <germanip@TR12.CAT>
2019-11-12 18:55:30 +0100

    LAB2 Exercise 1

--- LAB2_files/Eaa2rotMat.m
+++ LAB2_files/Eaa2rotMat.m
@@ -7,5 +7,10 @@ function [R] = Eaa2rotMat(a,u)
 % Outputs:
 %	R: generated rotation matrix
 
-end
 
+u = u/sqrt(u'*u);
+Ux = [0 u(3) -u(2); -u(3) 0 u(1); u(2) -u(1) 0]';
+Identidad = eye(3);
+
+R = Identidad*cosd(a) + (1-cosd(a))*(u*u')+Ux*sind(a);
+end
