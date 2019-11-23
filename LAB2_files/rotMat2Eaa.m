f34e8d51b3451397d81b3530bc753bda67e1f08c
Ap0lly0n <47557365+NotMortarion@users.noreply.github.com>
2019-11-23 19:07:27 +0100

    toy doramio

--- LAB2_files/rotMat2Eaa.m
+++ LAB2_files/rotMat2Eaa.m
@@ -8,10 +8,17 @@ function [a,u] = rotMat2Eaa(R)
 %	u: axis of rotation 
 
 a = acosd((trace(R)-1)/2);
+
 Rx = (R-R')/(2*sind(a));
 u = [Rx(3,2),Rx(1,3),Rx(2,1)]';
 if(a == 0)
     u = R(:,1);
 end
+if(a == 180)
+    u(1) = sqrt((R(1,1)+1)/2);
+    u(2) = -sqrt((R(2,2)+1)/2);
+    u(3) = sqrt((R(3,3)+1)/2);
+end
+
 end
 
